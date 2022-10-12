0711df79edbc97eb9b28276ed0bbf29f037f0679
João Victor Morais <jv.moraiscb@gmail.com>
2022-10-12 11:54:23 -0300

    :apple: Add player physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,64 @@ public class Player : MonoBehaviour
     private SpriteRenderer sr;
     private Animator anim;
     private string WALK_ANIMATION = "Walk";
+    private string JUMP_ANIMATION = "Jump";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerJump();
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
 
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void AnimatePlayer()
+    {
+
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
     }
 }
